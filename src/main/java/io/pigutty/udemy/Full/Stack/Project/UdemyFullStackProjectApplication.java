ea6e2a862dc4e9739e6a0b092d59680f592529d0
pigutty <50047826+pigutty@users.noreply.github.com>
2019-09-05 15:30:44 +0900

    initial commit

---
+++ src/main/java/io/pigutty/udemy/Full/Stack/Project/UdemyFullStackProjectApplication.java
@@ -0,0 +1,13 @@
+package io.pigutty.udemy.Full.Stack.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyFullStackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyFullStackProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pigutty/udemy/Full/Stack/Project/UdemyFullStackProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pigutty.udemy.Full.Stack.Project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdemyFullStackProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
