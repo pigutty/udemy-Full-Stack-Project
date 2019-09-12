e5702eb921377e14fbcf3349e4099d7ff8f08815
pigutty <pigutty.tech@gmail.com>
2019-09-12 14:05:33 +0900

    error handling when updating project

--- udemy-full-stack-react-client/src/components/Project/AddProject.js
+++ udemy-full-stack-react-client/src/components/Project/AddProject.js
@@ -95,7 +95,7 @@ class AddProject extends Component {
                     value={this.state.description}
                     onChange={this.onChange}
                   ></textarea>{" "}
-                  {errors.projectName && (
+                  {errors.description && (
                     <div className="invalid-feedback">{errors.description}</div>
                   )}
                 </div>{" "}
