d80007abb9be1506bb324c2bc36c5c93e9031055
pigutty <pigutty.tech@gmail.com>
2019-09-12 16:59:27 +0900

    Entity Relationships: Project and Backlog

--- src/main/java/io/pigutty/udemy/Full/Stack/Project/services/ProjectService.java
+++ src/main/java/io/pigutty/udemy/Full/Stack/Project/services/ProjectService.java
@@ -1,7 +1,9 @@
 package io.pigutty.udemy.Full.Stack.Project.services;
 
+import io.pigutty.udemy.Full.Stack.Project.domain.Backlog;
 import io.pigutty.udemy.Full.Stack.Project.domain.Project;
 import io.pigutty.udemy.Full.Stack.Project.exceptions.ProjectIdException;
+import io.pigutty.udemy.Full.Stack.Project.repositories.BacklogRepository;
 import io.pigutty.udemy.Full.Stack.Project.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,26 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOrUpdateProject(Project project){
         try{
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if(project.getId()==null){
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
+            if(project.getId()!=null){
+                project.getBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
             return projectRepository.save(project);
+
         }catch (Exception e){
             throw new ProjectIdException("Project ID"+project.getProjectIdentifier().toUpperCase()+"already exists");
         }
