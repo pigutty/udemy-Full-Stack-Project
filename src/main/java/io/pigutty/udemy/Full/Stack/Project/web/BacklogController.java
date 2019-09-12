54c74b50019e4cfb0aa217695dd24241c562333f
pigutty <pigutty.tech@gmail.com>
2019-09-12 19:08:43 +0900

    create project task

---
+++ src/main/java/io/pigutty/udemy/Full/Stack/Project/web/BacklogController.java
@@ -0,0 +1,34 @@
+package io.pigutty.udemy.Full.Stack.Project.web;
+
+import io.pigutty.udemy.Full.Stack.Project.domain.ProjectTask;
+import io.pigutty.udemy.Full.Stack.Project.services.MapValidationErrorService;
+import io.pigutty.udemy.Full.Stack.Project.services.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/{backlogId}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask,
+                                            BindingResult result, @PathVariable String backlogId){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
+
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlogId, projectTask);
+
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+}
