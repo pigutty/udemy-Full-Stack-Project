2223e3fc62a6a91eda1fbcde1e3f63ff41d7c6f3
pigutty <pigutty.tech@gmail.com>
2019-09-12 20:56:56 +0900

    Handle Project not found exception

---
+++ src/main/java/io/pigutty/udemy/Full/Stack/Project/exceptions/ProjectNotFoundExceptionResponse.java
@@ -0,0 +1,17 @@
+package io.pigutty.udemy.Full.Stack.Project.exceptions;
+
+public class ProjectNotFoundExceptionResponse {
+    private String ProjectNotFound;
+
+    public ProjectNotFoundExceptionResponse(String projectNotFound){
+        ProjectNotFound = projectNotFound;
+    }
+
+    public String getProjectNotFound() {
+        return ProjectNotFound;
+    }
+
+    public void setProjectNotFound(String projectNotFound) {
+        ProjectNotFound = projectNotFound;
+    }
+}
