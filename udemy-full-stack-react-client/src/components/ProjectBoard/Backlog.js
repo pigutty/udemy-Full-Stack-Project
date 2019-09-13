c3bf9704ea3cd237741bced4e525647c9c3a71a0
pigutty <pigutty.tech@gmail.com>
2019-09-13 20:40:40 +0900

    organize project tasks by status and priority

--- udemy-full-stack-react-client/src/components/ProjectBoard/Backlog.js
+++ udemy-full-stack-react-client/src/components/ProjectBoard/Backlog.js
@@ -9,6 +9,22 @@ class Backlog extends Component {
       <ProjectTask key={project_task.id} project_task={project_task} />
     ));
 
+    let toDoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].props.project_task.status === "TO_DO") {
+        toDoItems.push(tasks[i]);
+      }
+      if (tasks[i].props.project_task.status === "IN_PROGRESS") {
+        inProgressItems.push(tasks[i]);
+      }
+      if (tasks[i].props.project_task.status === "DONE") {
+        doneItems.push(tasks[i]);
+      }
+    }
+
     return (
       <div className="container">
         <div className="row">
@@ -18,7 +34,7 @@ class Backlog extends Component {
                 <h3>TO DO</h3>
               </div>
             </div>
-            {tasks}
+            {toDoItems}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
@@ -26,6 +42,7 @@ class Backlog extends Component {
                 <h3>In Progress</h3>
               </div>
             </div>
+            {inProgressItems}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
@@ -33,6 +50,7 @@ class Backlog extends Component {
                 <h3>Done</h3>
               </div>
             </div>
+            {doneItems}
           </div>
         </div>
       </div>
