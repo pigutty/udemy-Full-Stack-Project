24d493afc2e7c2e22f92a866f963466d86020c15
pigutty <pigutty.tech@gmail.com>
2019-09-13 20:14:32 +0900

    load project_tasks to UI

--- udemy-full-stack-react-client/src/actions/backlogActions.js
+++ udemy-full-stack-react-client/src/actions/backlogActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_BACKLOG } from "./types";
 
 export const addProjectTask = (
   backlog_id,
@@ -15,4 +15,14 @@ export const addProjectTask = (
       payload: error.response.data
     });
   }
+};
+
+export const getBacklog = backlog_id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/backlog/${backlog_id}`);
+    dispatch({
+      type: GET_BACKLOG,
+      payload: res.data
+    });
+  } catch (error) {}
 };
