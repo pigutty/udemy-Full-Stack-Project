4196b73db421b2f6f2a4eb181e9a1e817cee35a6
pigutty <pigutty.tech@gmail.com>
2019-09-14 22:17:26 +0900

    パスの修正

--- udemy-full-stack-react-client/src/components/Layout/Landing.js
+++ udemy-full-stack-react-client/src/components/Layout/Landing.js
@@ -14,13 +14,10 @@ class Landing extends Component {
                   Create your account to join active projects or start you own
                 </p>
                 <hr />
-                <Link
-                  to="register.html"
-                  className="btn btn-lg btn-primary mr-2"
-                >
+                <Link to="/register" className="btn btn-lg btn-primary mr-2">
                   Sign Up
                 </Link>
-                <Link to="login.html" className="btn btn-lg btn-secondary mr-2">
+                <Link to="/login" className="btn btn-lg btn-secondary mr-2">
                   Login
                 </Link>
               </div>
