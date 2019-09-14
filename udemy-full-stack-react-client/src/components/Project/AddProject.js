2cd0238b794429bcf3c48956ee85471c7cc95ffe
pigutty <pigutty.tech@gmail.com>
2019-09-14 18:02:37 +0900

    lock operations to specific user

--- udemy-full-stack-react-client/src/components/Project/AddProject.js
+++ udemy-full-stack-react-client/src/components/Project/AddProject.js
@@ -14,6 +14,7 @@ class AddProject extends Component {
       description: "",
       start_date: "",
       end_date: "",
+      create_at: "",
       errors: {}
     };
     this.onChange = this.onChange.bind(this);
@@ -38,7 +39,8 @@ class AddProject extends Component {
       projectIdentifier: this.state.projectIdentifier,
       description: this.state.description,
       start_date: this.state.start_date,
-      end_date: this.state.end_date
+      end_date: this.state.end_date,
+      create_at: this.state.create_at
     };
     this.props.createProject(newProject, this.props.history);
   }
