98657641d8e07cbb4681d252538ad6de925a65af
pigutty <pigutty.tech@gmail.com>
2019-09-14 14:54:10 +0900

    JwtToken is generated!!!

--- src/main/java/io/pigutty/udemy/Full/Stack/Project/web/UserController.java
+++ src/main/java/io/pigutty/udemy/Full/Stack/Project/web/UserController.java
@@ -1,19 +1,27 @@
 package io.pigutty.udemy.Full.Stack.Project.web;
 
 import io.pigutty.udemy.Full.Stack.Project.domain.User;
+import io.pigutty.udemy.Full.Stack.Project.exceptions.payload.JWTLoginSucessResponse;
+import io.pigutty.udemy.Full.Stack.Project.exceptions.payload.LoginRequest;
+import io.pigutty.udemy.Full.Stack.Project.security.JwtAuthenticationEntryPoint;
+import io.pigutty.udemy.Full.Stack.Project.services.JwtTokenProvider;
 import io.pigutty.udemy.Full.Stack.Project.services.MapValidationErrorService;
 import io.pigutty.udemy.Full.Stack.Project.services.UserService;
 import io.pigutty.udemy.Full.Stack.Project.validator.UserValidator;
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import static io.pigutty.udemy.Full.Stack.Project.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -27,6 +35,31 @@ public class UserController {
 
     @Autowired
     private UserValidator userValidator;
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSucessResponse(true, jwt));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
