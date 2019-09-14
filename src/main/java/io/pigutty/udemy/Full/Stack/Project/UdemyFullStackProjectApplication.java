5a34abf9619dc8e96f85c9a3e2b1efa248fd680d
pigutty <pigutty.tech@gmail.com>
2019-09-14 12:25:14 +0900

    create user by username, fullname, password

--- src/main/java/io/pigutty/udemy/Full/Stack/Project/UdemyFullStackProjectApplication.java
+++ src/main/java/io/pigutty/udemy/Full/Stack/Project/UdemyFullStackProjectApplication.java
@@ -2,9 +2,15 @@ package io.pigutty.udemy.Full.Stack.Project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class UdemyFullStackProjectApplication {
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UdemyFullStackProjectApplication.class, args);
