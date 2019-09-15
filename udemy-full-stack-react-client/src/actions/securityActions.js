502a8beadd812d8940d6b53634c4b72b3cd117f8
pigutty <pigutty.tech@gmail.com>
2019-09-15 20:19:38 +0900

    login機能実装

--- udemy-full-stack-react-client/src/actions/securityActions.js
+++ udemy-full-stack-react-client/src/actions/securityActions.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setJWTToken from "../securityUtils/setJWTToken";
+import jwt_decode from "jwt-decode";
 
 export const createNewUser = (newUser, history) => async dispatch => {
   try {
@@ -8,6 +10,25 @@ export const createNewUser = (newUser, history) => async dispatch => {
     dispatch({
       type: GET_ERRORS,
       payload: {}
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data
+    });
+  }
+};
+
+export const login = LoginRequest => async dispatch => {
+  try {
+    const res = await axios.post("/api/users/login");
+    const { token } = res.data;
+    localStorage.setItem("jwtToken", token);
+    setJWTToken(token);
+    const decode = jwt_decode(token);
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: decode
     });
   } catch (error) {
     dispatch({
